88f2179f80f4fa9534947050faf27d8b749391d2
Engin.Diri <engin.diri@addison.de>
2016-06-16 13:55:35 +0200

    add angualr component

--- src/js/main.ts
+++ src/js/main.ts
@@ -1,5 +1,34 @@
-import $ from 'github:components/jquery@2.2.4';
-import bootstrap from 'twbs/bootstrap';
+//import $ from 'github:components/jquery@2.2.4';
+//import bootstrap from 'twbs/bootstrap';
 import 'twbs/bootstrap/css/bootstrap.css!';
+import 'reflect-metadata';
+import 'zone.js';
+import { bootstrap } from 'angular2/platform/browser';
+import { Component } from 'angular2/core';
 
-console.log('is this thing on?')
+export class Hero {
+  id: number;
+  name: string;
+}
+
+@Component({
+  selector: 'my-app',
+  template: `
+    <h1>{{title}}</h1>
+    <h2>{{hero.name}} details!</h2>
+    <div><label>id: </label>{{hero.id}}</div>
+    <div>
+      <label>name: </label>
+      <input [(ngModel)]="hero.name" placeholder="name">
+    </div>
+    `
+})
+export class MainComponent {
+  title = 'Tour of Heroesdsd';
+  hero: Hero = {
+    id: 1,
+    name: 'Windstorm'
+  };
+}
+
+bootstrap(MainComponent);
